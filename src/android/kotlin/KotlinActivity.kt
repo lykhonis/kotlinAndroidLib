a9d03effdbb966d3efdc9d6d22540ff546dcb187
Vladimir Lichonos <vladimirlichonos@gmail.com>
2012-05-23 15:49:38 -0700

    IDEA

--- src/android/kotlin/KotlinActivity.kt
+++ src/android/kotlin/KotlinActivity.kt
@@ -4,3 +4,5 @@ import android.app.Activity
 import android.view.View
 
 fun Activity.findView<T: View>(id: Int) = LazyView<T>(this, id)
+
+fun Activity.runOnUiThread(f: () -> Unit) = runOnUiThread(runnable(f))
