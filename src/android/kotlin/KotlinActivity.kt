bc7046b8cc7af66e229d3b91a98e70e510fdf673
Vladimir Lichonos <vladimirlichonos@gmail.com>
2012-06-04 11:47:05 -0700

    BroadcastReceiver and refactoring

--- src/android/kotlin/KotlinActivity.kt
+++ src/android/kotlin/KotlinActivity.kt
@@ -5,6 +5,6 @@ import android.view.View
 
 public inline fun Activity.findView<T: View>(id: Int): T? = findViewById(id) as? T
 
-public inline fun Activity.runOnUiThread(f: () -> Unit): Unit {
-    runOnUiThread(runnable(f))
+public inline fun Activity.runOnUiThread(action: () -> Unit): Unit {
+    runOnUiThread(runnable(action))
 }
