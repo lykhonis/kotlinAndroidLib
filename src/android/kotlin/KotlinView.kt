5147286c1627b0914c24d5e929459be086dd56e7
Vladimir Lichonos <vladimirlichonos@gmail.com>
2012-06-04 09:16:54 -0700

    FindViewById

--- src/android/kotlin/KotlinView.kt
+++ src/android/kotlin/KotlinView.kt
@@ -4,6 +4,8 @@ import android.view.View
 import android.app.Activity
 import android.view.View.OnClickListener
 
+fun View.findView<T: View>(id: Int) = findViewById(id) as? T
+
 fun viewOnClickListener(action: (View?) -> Unit) = object : OnClickListener {
     public override fun onClick(p0: View?) {
         action(p0)
