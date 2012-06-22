9a93f44c87d188cf8749ede9dd9e815931059db4
Vladimir Lichonos <vladimirlichonos@gmail.com>
2012-06-22 16:07:46 -0700

    Cursor nullable fix

--- src/com/vlad/android/kotlin/Cursor.kt
+++ src/com/vlad/android/kotlin/Cursor.kt
@@ -6,17 +6,11 @@ import java.util.LinkedList
 import java.util.Collection
 
 public inline fun <T> Cursor?.map(transform: Cursor.() -> T): Collection<T>  {
-    return (if (this == null) Collections.emptyList<T>() else
-        Collections.unmodifiableCollection(mapTo(LinkedList<T>(), transform))).sure()
+    return if (this == null) Collections.emptyList<T>().sure() else map(transform)
 }
 
-public inline fun <T, C: Collection<in T>> Cursor?.mapTo(result: C, transform: Cursor.() -> T): C {
-    if (this != null)
-        if (this.moveToFirst())
-            do {
-                result.add(this.transform())
-            } while (this.moveToNext())
-    return result
+public inline fun <T, C: Collection<T>> Cursor?.mapTo(result: C, transform: Cursor.() -> T): C {
+    return if (this == null) result else mapTo(result, transform)
 }
 
 public inline fun <T> Cursor?.mapAndClose(create: Cursor.() -> T): Collection<T> {
@@ -24,5 +18,25 @@ public inline fun <T> Cursor?.mapAndClose(create: Cursor.() -> T): Collection<T>
         return map(create)
     } finally {
         this?.close()
+    }
+}
+
+public inline fun <T> Cursor.map(transform: Cursor.() -> T): Collection<T>  {
+    return Collections.unmodifiableCollection(mapTo(LinkedList<T>(), transform)).sure()
+}
+
+public inline fun <T, C: Collection<T>> Cursor.mapTo(result: C, transform: Cursor.() -> T): C {
+    if (moveToFirst())
+        do {
+            result.add(transform())
+        } while (moveToNext())
+    return result
+}
+
+public inline fun <T> Cursor.mapAndClose(create: Cursor.() -> T): Collection<T> {
+    try {
+        return map(create)
+    } finally {
+        close()
     }
 }
